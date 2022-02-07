80577c0dafd23d10265c134bde09229e106e2e88
Max Gautier <mg@max.gautier.name>
2022-02-07 13:38:21 +0100

    Branch the server on Storage functions

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,18 @@ import Servant
 import Network.Wai.Handler.Warp
 import Control.Monad.IO.Class
 import Data.ByteString
+import Data.Functor
 
 server :: Server ObjectStore
 server x i = put :<|> get :<|> delete
     where
     object_path =  x <> "/" <> show i
 
-    put object_data = (liftIO . O.store object_path) object_data *> pure NoContent
+    put object_data = (liftIO . O.store x i) object_data $> NoContent
 
-    get = liftIO . O.retrieve $ object_path
+    get = liftIO $ O.retrieve x i
 
-    delete = (liftIO . O.delete) object_path *> pure NoContent
+    delete = liftIO (O.delete x i) $> NoContent
 
 objectApi :: Proxy ObjectStore
 objectApi = Proxy
