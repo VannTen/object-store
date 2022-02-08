f6f412afef25438e8f12cbf754fe05fd526492b3
Max Gautier <mg@max.gautier.name>
2022-02-08 01:57:02 +0100

    auto-formatter

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,17 @@
 module Main where
 
 import Api
-import Storage as O
-
-import Servant
-import Network.Wai.Handler.Warp
 import Control.Monad.IO.Class
 import Data.ByteString
 import Data.Functor
+import Network.Wai.Handler.Warp
 import Options.Applicative
+import Servant
+import Storage as O
 
 server :: Server ObjectStore
 server x i = put :<|> get :<|> delete
-    where
+  where
     put object_data = (liftIO . O.store x i) object_data $> Id i
 
     get = liftIO (O.ifObj x i O.retrieve) >>= found
@@ -21,8 +20,8 @@ server x i = put :<|> get :<|> delete
 
 found :: Maybe a -> Servant.Handler a
 found res = case res of
-        Nothing -> throwError err404
-        Just response -> pure response
+  Nothing -> throwError err404
+  Just response -> pure response
 
 objectApi :: Proxy ObjectStore
 objectApi = Proxy
@@ -31,14 +30,17 @@ app :: Application
 app = serve objectApi server
 
 port :: Parser Int
-port = option auto
-                (long "port" <>
-                short 'p' <>
-                metavar "PORT" <>
-                help "Port to bind on" <>
-                value 8080)
+port =
+  option
+    auto
+    ( long "port"
+        <> short 'p'
+        <> metavar "PORT"
+        <> help "Port to bind on"
+        <> value 8080
+    )
 
 main :: IO ()
 main = do
-    port <- execParser (info (port <**> helper) $ header "Simple object storage server")
-    run port app
+  port <- execParser (info (port <**> helper) $ header "Simple object storage server")
+  run port app
--- src/Storage.hs
+++ src/Storage.hs
@@ -1,41 +1,47 @@
 module Storage where
 
-import Data.ByteString as B
-import Data.Hashable
-import System.Posix
-import System.Directory.Extra
 import Control.Monad.Extra
-import Data.Function.Slip
+import Data.ByteString as B
 import Data.Composition
+import Data.Function.Slip
+import Data.Hashable
+import System.Directory.Extra
+import System.Posix
 
 type Bucket = String
+
 type ObjID = Int
+
 type Content = ByteString
 
 store :: Bucket -> ObjID -> B.ByteString -> IO ()
 store bucket obj content =
-    whenM (not <$> objIdentical bucket obj content) $
-    ensureBucketExists bucket <>
-    whenM (not <$> isDup) store <>
-    whenM isOverride unref <>
-    ref
-                where
-                isOverride = fileExist $ path bucket obj "/objects/"
-                isDup = (fileExist .: data_path) bucket content
-                obj_paths = path bucket obj
-                storage = data_path bucket content
-                store = B.writeFile storage content
-                ref = createLink storage (obj_paths "/objects/")
-                      <> createSymbolicLink storage (obj_paths "/refs/")
-                unref = removeLink (obj_paths "/objects/")
-                        <> whenM ((== 1) <$> exemplarOf bucket obj)
-                           (unstore $ obj_paths "/refs/")
-                        <> removeLink (obj_paths "/refs/")
+  whenM (not <$> objIdentical bucket obj content) $
+    ensureBucketExists bucket
+      <> whenM (not <$> isDup) store
+      <> whenM isOverride unref
+      <> ref
+  where
+    isOverride = fileExist $ path bucket obj "/objects/"
+    isDup = (fileExist .: data_path) bucket content
+    obj_paths = path bucket obj
+    storage = data_path bucket content
+    store = B.writeFile storage content
+    ref =
+      createLink storage (obj_paths "/objects/")
+        <> createSymbolicLink storage (obj_paths "/refs/")
+    unref =
+      removeLink (obj_paths "/objects/")
+        <> whenM
+          ((== 1) <$> exemplarOf bucket obj)
+          (unstore $ obj_paths "/refs/")
+        <> removeLink (obj_paths "/refs/")
 
-
-objIdentical b obj content = fileExist (path b obj "/objects/")
-                                  &&^ fmap (== data_path b content)
-                                      (readSymbolicLink $ path b obj "/refs/")
+objIdentical b obj content =
+  fileExist (path b obj "/objects/")
+    &&^ fmap
+      (== data_path b content)
+      (readSymbolicLink $ path b obj "/refs/")
 
 unstore = removeLink <=< readSymbolicLink
 
@@ -43,29 +49,35 @@ retrieve :: Bucket -> ObjID -> IO ByteString
 retrieve = B.readFile .: slipl path "/objects/"
 
 delete :: Bucket -> ObjID -> IO ()
-delete bucket obj = whenM ((== 2) <$> exemplarOf bucket obj) (unstore data_ref)
-                       <> unref
-                where
-                obj_paths = path bucket obj
-                data_ref = obj_paths "/refs/"
-                unref = removeLink (obj_paths "/objects/")
-                     <> removeLink data_ref
+delete bucket obj =
+  whenM ((== 2) <$> exemplarOf bucket obj) (unstore data_ref)
+    <> unref
+  where
+    obj_paths = path bucket obj
+    data_ref = obj_paths "/refs/"
+    unref =
+      removeLink (obj_paths "/objects/")
+        <> removeLink data_ref
 
-ifObj bucket obj f = whenMaybeM
-                        (fileExist $ path bucket obj "/objects/") $
-                        f bucket obj
+ifObj bucket obj f =
+  whenMaybeM
+    (fileExist $ path bucket obj "/objects/")
+    $ f bucket obj
 
 data_path :: Bucket -> Content -> FilePath
 data_path bucket content = bucket <> "/store/" <> (show . hash) content
 
 exemplarOf :: Bucket -> ObjID -> IO LinkCount
-exemplarOf b i = fmap linkCount $
-                 getFileStatus <=< readSymbolicLink $
-                 b <> "/refs/" <> show i
+exemplarOf b i =
+  fmap linkCount $
+    getFileStatus <=< readSymbolicLink $
+      b <> "/refs/" <> show i
 
-path ::  Bucket -> ObjID -> String -> FilePath
-path b obj what =  b <> what <> show obj
+path :: Bucket -> ObjID -> String -> FilePath
+path b obj what = b <> what <> show obj
 
-ensureBucketExists bucket = foldMap (
-                                createDirectoryIfMissing True . (bucket <>))
-                                ["/refs/", "/objects/", "/store/"]
+ensureBucketExists bucket =
+  foldMap
+    ( createDirectoryIfMissing True . (bucket <>)
+    )
+    ["/refs/", "/objects/", "/store/"]
